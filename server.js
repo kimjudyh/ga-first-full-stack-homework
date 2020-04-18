ab308457d30b363d0d9e9fff13e07ef99972c3b6
Judy <kimjudyh@gmail.com>
2020-04-18 14:56:04 -0700

    imported and set up view engine and middleware

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // ============ IMPORTS
 const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 // =========== VIEW ENGINE
+app.set('view engine', 'ejs');
 
 // =========== CONTROLLERS
 
 // =========== MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 // ---------- Custom Middleware
+app.use((req, res, next) => {
+  console.log(`Request received ${req.url}`);
+  next();
+})
 
 // =========== ROUTES
 app.get('/', (req, res) => {
