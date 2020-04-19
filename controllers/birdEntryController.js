303a407fd7841bec42b6b466f71aef66b66ddc65
Judy <kimjudyh@gmail.com>
2020-04-19 14:27:14 -0700

    made bird seen entry edit route, view

--- controllers/birdEntryController.js
+++ controllers/birdEntryController.js
@@ -41,8 +41,48 @@ entryRouter.post('/:id/entry', (req, res) => {
 })
 
 // GET edit route
+entryRouter.get('/:id/entry/:entryId/edit', (req, res) => {
+  db.Bird.findOne({_id: req.params.id, seen: {$elemMatch: {_id: req.params.entryId}}},
+     (err, foundBird) => {
+    if (err) {
+      console.log(err); res.redirect('/');
+    }
+    console.log(foundBird);
+    res.render('editEntry', {
+      id: req.params.id,
+      entry: foundBird.seen.id(req.params.entryId),
+      entryId: req.params.entryId,
+    });
+  })
+})
 
 // PUT update route
+entryRouter.put('/:id/entry/:entryId', (req, res) => {
+  let entryId = req.params.entryId;
+  console.log(req.body);
+  db.Bird.findByIdAndUpdate(req.params.id, {  }, () => {})
+  db.Bird.findOneAndUpdate(
+    { _id: req.params.id, "seen._id": entryId },
+    {
+      $set: {
+        "seen.$":
+        {
+          date: req.body.date,
+          location: req.body.location,
+          notes: req.body.notes,
+        }
+      }
+    },
+    { arrayFilters: [{ element: entryId }],
+     new: true },
+  (err, foundBird) => {
+    if (err) {
+      console.log(err); res.redirect('/');
+    }
+    console.log('found bird for entry', foundBird);
+    res.redirect(`/birds/${req.params.id}`);
+  })
+})
 
 // =========== EXPORTS
 module.exports = entryRouter;
